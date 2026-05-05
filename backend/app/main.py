53c1fb73a3ffdfe75fa64cdc2c0d07dd273697eb
YunTaeKyun <ytk5495@gmail.com>
2026-05-05 09:35:25 +0900

    sentry iniy

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,12 +3,23 @@ from fastapi.middleware.cors import CORSMiddleware
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
-
+import sentry_sdk
+from sentry_sdk.integrations.fastapi import FastApiIntegration
+from sentry_sdk.integrations.sqlalchemy import SqlalchemyIntegration
 from app.core.limiter import limiter
 from app.core.config import settings
 from app.routers import auth, user, health, challenge,character,dashboard, diet, predict, recommend,report
 
 
+sentry_sdk.init(
+    dsn = settings.SENTRY_DSN,
+    intergrations =[
+        FastApiIntergration(),
+        SqlalchemyIntegration()
+    ],
+    traces_sample_rate= 1.0,
+    environment=settings.APP_ENV,
+)
 
 app = FastAPI(
     title="당마고치 API",
